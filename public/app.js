d6b11a30b4c9cb6711941d8b184c4dba654ef3f1
Nathan Hall <nathan.mh@gmail.com>
2016-06-16 10:58:36 -0400

    naynay doing good work

---
+++ public/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+})
+
+var vespaPage  = {
+  init: function() {
+    vespaPage.events()
+    vespaPage.styling()
+  },
+
+  styling: function() {
+
+  },
+
+  events: function() {
+
+  },
+
+  create: function(vespaObject) {
+    $.ajax({
+      method: "POST",
+      url: "/vespa",
+      data: vespaObject,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err) {
+        console.error("OH SHIT, zach messed up", err);
+      }
+    })
+  },
+
+  read: function() {
+    $.ajax({
+      method: "GET",
+      url: "/vespa",
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err) {
+        console.error("OH SHIT, zach messed up", err);
+      }
+    })
+  },
+  update: function() {
+
+  },
+
+  destroy: function() {
+
+  }
+}
